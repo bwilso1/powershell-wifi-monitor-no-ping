21f8ea5743c9f9c7388d7189341d1b667d7ba75d
B Wilson <bwilso1@umbc.edu>
2018-03-29 19:03:54 -0400

    added timestamps to log file & console

--- inet_mon.ps1
+++ inet_mon.ps1
@@ -14,17 +14,22 @@ $wmi = Get-WmiObject -Class Win32_NetworkAdapter -filter "Name LIKE '%Wireless%'
 #dir to save log file
 $myDir = "C:\wifi_log.txt"
 
-Write-Output "begining script, press Ctrl + C to quit..."
+#custom spacing for timestamps
+$space = "`t`t`t`t`t`t`t`t`t`t`t`t"
+
+Write-Output "$(Get-Date -Format '%M/d/yy HH:mm:ss')`t begining script, press Ctrl + C to quit..."
+
 while ($wmi -ne $null){
     
 
     # wmi info page   https://msdn.microsoft.com/en-us/library/aa394216(v=vs.85).aspx
 
     if ($wmi.NetConnectionStatus -eq 1){
-        Write-output "adapter is connecting"
+        Write-output "$(Get-Date -Format '%M/d/yy HH:mm:ss')`t adapter is connecting"
+ 
 
     }elseif($wmi.NetConnectionStatus -eq 7){
-        Write-output "adapter was disabled"
+        Write-output "$(Get-Date -Format '%M/d/yy HH:mm:ss')`t adapter was disabled"
         $wmi.Enable()
 
     }else{
@@ -34,8 +39,9 @@ while ($wmi -ne $null){
             #this may work with win 8 and above, but I have windows 7
             #Disable-NetAdapter -Name "eduroam" -Confirm:$false
 
-            Write-Output "internet was down, resetting adapter" | Out-File $myDir -NoClobber -Append
-            Write-Output "internet was down, resetting adapter"
+            Write-Output "$(Get-Date -Format '%M/d/yy HH:mm:ss')`t internet was down, resetting adapter" | Out-File $myDir -NoClobber -Append
+            Write-Output "$(Get-Date -Format '%M/d/yy HH:mm:ss')`t internet was down, resetting adapter"
+           
             #this works with windows 7
             $wmi.Disable() | Out-File $myDir -NoClobber -Append
             $wmi.Enable()  | Out-File $myDir -NoClobber -Append
@@ -51,9 +57,9 @@ while ($wmi -ne $null){
     #write-output "end sleep2"
     $wmi = Get-WmiObject -Class Win32_NetworkAdapter -filter "Name LIKE '%Wireless%'"
     
-} 
+} #end main loop
 
-if ($wmi -eq $null) { Write-Output "Network Object was null, check name again"}
-Write-Output "Script end..."
+if ($wmi -eq $null) { Write-Output "$(Get-Date -Format '%M/d/yy HH:mm:ss')`t Network Object was null, check name again"}
+Write-Output "$(Get-Date -Format '%M/d/yy HH:mm:ss')`t Script end..."
 
 
